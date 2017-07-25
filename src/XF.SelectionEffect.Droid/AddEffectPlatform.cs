5850374b05f874687ec7cb202a170e89ab2be1f6
mrxten <mr.xten@ya.ru>
2017-07-25 16:34:42 +0300

    Added first version without ripple.

---
+++ src/XF.SelectionEffect.Droid/AddEffectPlatform.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Java.Util;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XF.SelectionEffect;
+using XF.SelectionEffect.Droid;
+using View = Android.Views.View;
+
+[assembly: ResolutionGroupName("SelectionEffect")]
+[assembly: ExportEffect(typeof(AddEffectPlatform), nameof(AddEffect))]
+namespace XF.SelectionEffect.Droid
+{
+    public class AddEffectPlatform : PlatformEffect
+    {
+        private Android.Views.View _view;
+        private FrameLayout _layer;
+        private Android.Graphics.Color _color;
+        private RippleDrawable _ripple;
+
+        protected override void OnAttached()
+        {
+            _view = Control ?? Container;
+
+            if (_view is Android.Widget.ListView)
+            {
+                //Except ListView because of Raising Exception OnClick
+                return;
+            }
+            _view.Touch += OnTouch;
+
+            UpdateEffectColor();
+        }
+
+        protected override void OnDetached()
+        {
+            _ripple?.Dispose();
+            _ripple = null;
+            _view.Touch -= OnTouch;
+        }
+
+        private void OnTouch(object sender, View.TouchEventArgs args)
+        {
+            if (args.Event.Action == MotionEventActions.Down)
+            {
+                Container.AddView(_layer);
+                _layer.Top = 0;
+                _layer.Left = 0;
+                _layer.Right = _view.Width;
+                _layer.Bottom = _view.Height;
+                _layer.BringToFront();
+            }
+            if (args.Event.Action == MotionEventActions.Up || args.Event.Action == MotionEventActions.Cancel)
+            {
+                Container.RemoveView(_layer);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(e);
+
+            if (e.PropertyName == AddEffect.ColorProperty.PropertyName)
+            {
+                UpdateEffectColor();
+            }
+        }
+
+        private void UpdateEffectColor()
+        {
+            _view.Touch -= OnTouch;
+            if (_layer != null)
+            {
+                _layer.Dispose();
+                _layer = null;
+            }
+
+            var color = AddEffect.GetColor(Element);
+            if (color == Color.Default)
+            {
+                return;
+            }
+            _color = color.ToAndroid();
+            _color.A = 75;
+
+            _layer = new FrameLayout(Container.Context);
+            _layer.LayoutParameters = new ViewGroup.LayoutParams(-1, -1);
+            _layer.SetBackgroundColor(_color);
+            _view.Touch += OnTouch;
+        }
+    }
+}
---
+++ src/XF.SelectionEffect.iOS/AddEffectPlatform.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XF.SelectionEffect;
+using XF.SelectionEffect.iOS;
+
+[assembly: ResolutionGroupName("SelectionEffect")]
+[assembly: ExportEffect(typeof(AddEffectPlatform), nameof(AddEffect))]
+namespace XF.SelectionEffect.iOS
+{
+    public class AddEffectPlatform : PlatformEffect
+    {
+        private UITapGestureRecognizer _tapGesture;
+        private UILongPressGestureRecognizer _longTapGesture;
+        private UIView _view;
+        private UIView _layer;
+        private double _alpha;
+
+        protected override void OnAttached()
+        {
+            _view = Control ?? Container;
+            UpdateEffectColor();
+
+            _tapGesture = new UITapGestureRecognizer(async (obj) => {
+                await TapAnimation(0.3, _alpha, 0);
+            });
+
+            _longTapGesture = new UILongPressGestureRecognizer(async (obj) => {
+                switch (obj.State)
+                {
+                    case UIGestureRecognizerState.Began:
+                        await TapAnimation(0.5, 0, _alpha, false);
+                        break;
+                    case UIGestureRecognizerState.Ended:
+                    case UIGestureRecognizerState.Cancelled:
+                    case UIGestureRecognizerState.Failed:
+                        await TapAnimation(0.5, _alpha);
+                        break;
+                }
+            });
+
+            _view.AddGestureRecognizer(_longTapGesture);
+            _view.AddGestureRecognizer(_tapGesture);
+        }
+
+        protected override void OnDetached()
+        {
+            _view.RemoveGestureRecognizer(_tapGesture);
+            _tapGesture.Dispose();
+
+            _view.RemoveGestureRecognizer(_longTapGesture);
+            _longTapGesture.Dispose();
+
+            if (_layer != null)
+            {
+                _layer.Dispose();
+                _layer = null;
+            }
+        }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(e);
+
+            if (e.PropertyName == AddEffect.ColorProperty.PropertyName)
+            {
+                UpdateEffectColor();
+            }
+        }
+
+        private void UpdateEffectColor()
+        {
+
+            if (_layer != null)
+            {
+                _layer.Dispose();
+                _layer = null;
+            }
+
+            var color = AddEffect.GetColor(Element);
+            if (color == Color.Default)
+            {
+                return;
+            }
+            _alpha = color.A < 1.0 ? 1 : 0.3;
+
+            _layer = new UIView {BackgroundColor = color.ToUIColor()};
+        }
+
+        async Task TapAnimation(double duration, double start = 1, double end = 0, bool remove = true)
+        {
+            if (_layer != null)
+            {
+                _layer.Frame = new CGRect(0, 0, Container.Bounds.Width, Container.Bounds.Height);
+                Container.AddSubview(_layer);
+                Container.BringSubviewToFront(_layer);
+                _layer.Alpha = (float)start;
+                await UIView.AnimateAsync(duration, () => {
+                    _layer.Alpha = (float)end;
+                });
+                if (remove)
+                {
+                    _layer.RemoveFromSuperview();
+                }
+            }
+        }
+    }
+}
