e7f6d421dfe716682641bf99fe2d3682913875cb
mrxten <mr.xten@ya.ru>
2017-07-25 19:05:52 +0300

    Forgotten change.

--- src/XamEffects.iOS/Initialize.cs
+++ src/XamEffects.iOS/Initialize.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Foundation;
-using UIKit;
-
-namespace XF.SelectionEffect.iOS
+﻿namespace XamEffects.iOS
 {
     public static class Effects
     {
