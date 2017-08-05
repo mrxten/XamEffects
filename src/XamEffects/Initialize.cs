d11587831cb865abf2e91d992760e2daf6282325
mrxten <mr.xten@ya.ru>
2017-08-05 10:22:49 +0300

    Added Init for linker in pcl.

---
+++ src/XamEffects/Initialize.cs
@@ -0,0 +1,11 @@
+﻿namespace XamEffects
+{
+    public static class Effects
+    {
+        public static void Init()
+        {
+            TouchEffect.Init();
+            Commands.Init();
+        }
+    }
+}
