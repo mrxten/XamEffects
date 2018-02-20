b7613063647d141d3869a04a9c0fc236ef05aa01
mrxten <mr.xten@ya.ru>
2018-02-20 10:39:59 +0300

    Add init in android.

--- src/XamEffects.Sample/XamEffects.Sample.Android/MainActivity.cs
+++ src/XamEffects.Sample/XamEffects.Sample.Android/MainActivity.cs
@@ -19,6 +19,8 @@ namespace XamEffects.Sample.Droid
 
             base.OnCreate(bundle);
 
+			XamEffects.Droid.Effects.Init();
+
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
