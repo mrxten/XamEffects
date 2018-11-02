4d457620596454eddb8d04fa819fb32ec8ae3768
Vladislav Zhukov <>
2018-11-02 12:38:54 +0300

    update example

---
+++ src/Test/XamExample/MyPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamExample {
+    public partial class MyPage : ContentPage {
+        public MyPage() {
+            InitializeComponent();
+
+            var c = 0;
+
+            XamEffects.TouchEffect.SetColor(plus, Color.White);
+            XamEffects.Commands.SetTap(plus, new Command(() => {
+                c++;
+                counter.Text = $"Touches: {c}";
+            }));
+
+            XamEffects.TouchEffect.SetColor(minus, Color.White);
+            XamEffects.Commands.SetTap(minus, new Command(() => {
+                c--;
+                counter.Text = $"Touches: {c}";
+            }));
+        }
+    }
+}
